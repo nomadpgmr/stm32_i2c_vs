d2003b753d5507351daa8a5e1cf9aa89cdb84845
unknown <rogerw@vendnovation.com>
2019-02-16 12:49:26 -0800

    Initial save, no new code

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include "mbed.h"
+
+// Serial pc(USBTX, USBRX);
+
+DigitalOut led1(LED1);
+
+class MyLed
+{
+   public:
+
+   MyLed()
+   {
+   }
+   ~MyLed()
+   {
+   }
+
+   void blink(float delay)
+   {
+      led1 = 1;
+      wait(delay);
+      led1 = 0;
+      led1 != 0;
+
+      wait(delay);
+   }
+   void run()
+   {
+      while (1)
+      {
+         blink(0.2);
+         blink(0.2);
+         blink(0.2);
+         blink(0.2);
+         blink(0.2);
+         wait(1.0);
+         blink(0.2);
+         wait(1.0);
+      };
+   }
+};
+
+int main()
+{
+   // Add breakpoint here to prevent exceptions
+   MyLed *myled = new MyLed();
+   // pc.printf("Hello World\r\n");
+   myled->run();
+   free(myled);
+
+}
